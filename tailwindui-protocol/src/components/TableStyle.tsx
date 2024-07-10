01968a8f9abdadfbffdb5cb9101ca97b943d227b
Todd Riley <todd@websbytodd.com>
2024-07-10 17:20:36 -0400

    Add share/branches page and assets (table styling still needs work).

--- tailwindui-protocol/src/components/TableStyle.tsx
+++ tailwindui-protocol/src/components/TableStyle.tsx
@@ -1,4 +1,4 @@
-const inlineStyle = `
+const inlineStyle2Col = `
 table th:first-of-type {
     width: 25%;
 }
@@ -7,9 +7,38 @@ table th:nth-of-type(2) {
 }
 `
 
+const inlineStyle3Col = `
+table {
+    height: fit-content;
+}
+table tr {
+    height: 100%;
+}
+table th:first-of-type {
+    width: 25%;
+}
+table th:nth-of-type(2) {
+    width: 50%;
+}
+table th:nth-of-type(3) {
+    width: 25%;
+}
+`
+
+interface TableStyleProps {
+  cols: number
+}
+
 // https://stackoverflow.com/a/58338258/6569520
-export function TableStyle() {
-  return (
-  <style dangerouslySetInnerHTML={{ __html: inlineStyle}}/>
-  )
+export function TableStyle({ cols }: TableStyleProps) {
+  let html: string
+  if (cols === 2) {
+    html = inlineStyle2Col
+  } else if (cols === 3) {
+    html = inlineStyle3Col
+  } else {
+    throw new Error('Unsupported number of Table columns')
+  }
+
+  return <style dangerouslySetInnerHTML={{ __html: html }} />
 }
