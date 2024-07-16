d9fd12b6093dcd5a5656508eb1fc9ab237cfb4ed
Todd Riley <todd@websbytodd.com>
2024-07-16 12:49:00 -0400

    Convert the branch types markdown table to its own component.

---
+++ next/src/components/Tables/BranchTable.tsx
@@ -0,0 +1,64 @@
+import Image from 'next/image'
+
+export function BranchTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="w-[25%]">Kind</th>
+          <th className="w-[75%]">What does it do?</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <Image
+              alt="Branch"
+              width="16"
+              height="16"
+              className="m-0 inline"
+              src="/branches/sticky-notes-and-paintbrushes/local-branch.png"
+            />{' '}
+            Local branch
+          </td>
+          <td>
+            Marks a moveable spot in history and makes new commits. There are
+            lots of ways to use a branch, and we'll show you all of them.
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Image
+              alt="Remote Branch"
+              width="16"
+              height="16"
+              className="m-0 inline"
+              src="/branches/sticky-notes-and-paintbrushes/remote-branch.png"
+            />{' '}
+            Remote branch
+          </td>
+          <td>
+            Keeps track of where a friend's branch is. You can't move it
+            directly - it gets moved automatically when you download or upload
+            from your friend's repository.
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Image
+              alt="Tag"
+              width="16"
+              height="16"
+              className="m-0 inline"
+              src="/branches/sticky-notes-and-paintbrushes/tag.png"
+            />{' '}
+            Tag
+          </td>
+          <td>
+            Marks a permanent spot in history. Really just a "permanent branch".
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
